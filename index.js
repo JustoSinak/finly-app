c1769566d3b92f160ae70deb1388b1009f9906c9
JustoSinak <tcheumanisinakjusto@gmail.com>
2024-12-12 05:44:52 +0100

    update vercel.json

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 
 const app = express();
 
+const path = require('path'); 
 app.set('views', path.join(__dirname, 'views')); // Ensure this points to the correct views folder
 app.set('view engine', 'ejs');
 
