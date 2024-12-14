d50845eb5f89f291102404099b8bed95279c3671
JustoSinak <tcheumanisinakjusto@gmail.com>
2024-12-14 20:36:12 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,42 +1,39 @@
-
 const express = require('express');
 const morgan = require('morgan');
-
-const userRouter = require('./routes/user.route');
-
+const session = require('express-session');
 
 require('dotenv').config();
 require('./libs/dbConnect');
 
+const userRouter = require('./routes/user.route');
+const dashboardRouter = require('./routes/dashboard.route');
+
 const app = express();
 
-const path = require('path'); 
-app.set('views', path.join(__dirname, 'views')); // Ensure this points to the correct views folder
+app.set('views', './views');
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(morgan('dev'));
+app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.render('index', { message: 'Hello From Node.js' });
-});
+app.use(
+  session({
+    secret: process.env.AUTH_SECRET,
+    saveUninitialized: true,
+    resave: false,
+  })
+);
 
-app.use('/users', userRouter);
+app.use('/', userRouter);
+app.use('/dashboard', dashboardRouter);
 
-
-app.get('/contact', (req, res) => {
-    res.render('index', { message: 'The Contact Page' });
-});
-app.get('/about', (req, res) => {
-    res.render('index', { message: 'The About Page' });
-});
 app.get('*', (req, res) => {
-    res.status(404).render('index', { message: 'Not Found' });
+  res.status(404).render('index', { message: 'Not Found' });
 });
 
+const PORT = 3002;
 
-const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
