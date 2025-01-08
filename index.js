4b9449d7917afc4d8afab3ab3cdff280961e8403
JustoSinak <tcheumanisinakjusto@gmail.com>
2025-01-08 19:13:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require('./libs/dbConnect');
 
 const userRouter = require('./routes/user.route');
 const dashboardRouter = require('./routes/dashboard.route');
+const { verifyUser } = require('./libs/middleware');
 
 const app = express();
 
@@ -29,7 +30,7 @@ app.use(
 app.use(flash());
 // console.log(path.join(__dirname, 'views'));
 app.use('/', userRouter);
-app.use('/dashboard', dashboardRouter);
+app.use('/dashboard',verifyUser, dashboardRouter);
 
 app.get('*', (req, res) => {
   res.status(404).render('index', { message: 'Not Found' });
